e82a33753aaa99ef4b1e1ea6666770cff0abc43f
Rasim <Rasim467@mail.ru>
2021-05-05 13:38:50 +0400

    1. Динамический список на массиве. [#528503]

---
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,56 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private T[] data;
+    private int size;
+    private int modCount;
+
+    public SimpleArray() {
+        data = (T[]) new Object[10];
+    }
+    public SimpleArray(int capacity) {
+        data = (T[]) new Object[capacity];
+    }
+
+    public T get(int index) {
+        return data[Objects.checkIndex(index, size)];
+    }
+
+    public void add(T model) {
+        if (size == data.length) {
+            arrayGrow();
+        }
+        data[size++] = model;
+        modCount++;
+    }
+
+    private void arrayGrow() {
+        data = Arrays.copyOf(data, data.length + data.length / 2);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int point = 0;
+            final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                return point < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return data[point++];
+            }
+        };
+    }
+}
